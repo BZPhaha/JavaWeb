e2a7056652f59570b1ee0bea85ecb596431fde49
momo919 <13682018723@163.com>
2017-11-15 17:25:20 +0800

    测试实施233

--- src/ajaxtest/Test1115.java
+++ src/ajaxtest/Test1115.java
@@ -4,5 +4,9 @@ package ajaxtest;
  * Created by 13682 on 2017/11/15.
  */
 public class Test1115 {
-    public  int NUM = 111111111;
+    public  int NUM1 = 111111111;
+    public  int NUM2 = 111111111;
+    public  int NUM3 = 111111111;
+    public  int NUM4 = 111111111;
+
 }
